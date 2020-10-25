6156f1f53bee43818af8ac6a03a5afae420b4a25
Yash Saravgi <yashsaravgi911@gmail.com>
2020-10-26 01:14:30 +0530

    heroku deploy

--- backend/server.js
+++ backend/server.js
@@ -24,6 +24,15 @@ const usersRouter = require('./routes/users');
 app.use('/exercises', exercisesRouter);
 app.use('/users', usersRouter);
 
+if (process.env.NODE_ENV == "production") {
+
+	app.use(express.static("./build"));
+
+	app.get("*", (req, res) => {
+		res.sendFile(path.resolve(__dirname, "./exam-tracker", "build", "index.html"));
+	});
+}
+
 app.listen(port, () => {
     console.log(`Server is running on port: ${port}`);
 });
